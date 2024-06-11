b9acf0321f7a2793e09f17fdceaa385decd14e4d
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-06-11 11:36:13 -0300

    feat: login and login button components created.

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { InputLogin } from "./components/InputLogin";
+import { ButtonLogin } from "./components/ButtonLogin";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleEntrar = () => {
+    const emailLength = useMemo(() => {
+        return email.length * 1000;
+    }, [email.length]);
 
-    }
+    const handleEntrar = useCallback(() => {
+        console.log(email)
+        console.log(password)
+    }, [email, password]);
 
     return (
         <div>
             <form>
-                <label>
-                    <span>Email</span>
-                    <input value={email} onChange = {e => setEmail(e.target.value)} />
-                </label>
-                <label>
-                    <span>Senha</span>
-                    <input value={password} onChange = {e => setPassword(e.target.value)} />
-                </label>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
 
-                <button type = "button" onClick = {handleEntrar}>
+                <InputLogin 
+                    label = "Email"
+                    value = {email}
+                    onChange = {newValue => setEmail(newValue)}   
+                    onPressEnter = {() => inputPasswordRef.current?.focus()}
+                />
+
+                <InputLogin 
+                    type = "password"
+                    label = "Senha"
+                    value = {password}
+                    ref = {inputPasswordRef}
+                    onChange = {newValue => setPassword(newValue)}
+                />
+
+               <ButtonLogin type = "button" onClick = {handleEntrar}>
                     Entrar
-                </button>
+               </ButtonLogin>
             </form>
         </div>
     );
