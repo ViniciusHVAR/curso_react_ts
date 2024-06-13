cc2e8806e36e578b0ca37a313065db0124a76447
ViniciusHVAR <vinicius.martins@hvarconsulting.com.br>
2024-06-13 18:51:18 -0300

    feat: listas, listas de objetos, JSON server, consultas de API.

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,26 +1,69 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
-import { useUsuarioLogado } from "../../shared/hooks";
+import { useCallback, useState } from "react";
+
+interface ITarefa {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+}
 
 export const Dashboard = () => {
-    const counterRef = useRef(0);
+    const [lista, setLista] = useState<ITarefa[]>([]);
 
-    const { nomeDoUsuario, logout } = useUsuarioLogado();
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key === 'Enter') {
+            if (e.currentTarget.value.trim().length === 0) return;
+            
+            const value = e.currentTarget.value;
+
+            e.currentTarget.value = '';
+
+            setLista((oldLista) => {
+                if (oldLista.some((listItem) => listItem.title === value)) return oldLista;
+
+                return [
+                    ...oldLista,
+                    {
+                        id: oldLista.length,
+                        title: value,
+                        isCompleted: false,
+                    }
+                ];
+            });
+        }
+    }, []);
 
     return (
         <div>
-            <p>Dashboard</p>
+            <p>Lista</p>
 
-            <p>{nomeDoUsuario}</p>
+            <input onKeyDown = {handleInputKeyDown}/>
 
-            <p>Contador: {counterRef.current}</p>
+            <p>{lista.filter((listItem) => listItem.isCompleted).length}</p>
 
-            <button onClick={() => counterRef.current++}>Somar</button>
-            <button onClick={() => console.log(counterRef.current)}>Log</button>
+            <ul>
+                {lista.map((listItem) => {
+                    return <li key = {listItem.id}>
+                        <input 
+                            type="checkbox"
+                            checked = {listItem.isCompleted} 
+                            onChange = {() => {
+                                setLista(oldLista => {
+                                    return oldLista.map(oldListItem => {
+                                        const newIsCompleted = oldListItem.title === listItem.title
+                                        ? !oldListItem.isCompleted
+                                        : oldListItem.isCompleted
+                                        return {
+                                            ...oldListItem,
+                                            isCompleted: newIsCompleted,
+                                        }
+                                    });
+                                });
+                            }}
+                        />
+                        {listItem.title}</li>
+                })}
+            </ul>
 
-            <button onClick={logout}>Logout</button>
-
-            <Link to = "/login">Login</Link>
         </div>
     );
 }
